a964017fcd5c3a87fca84dba344dc84a117d9148
Jiří Bažant <j.bazant@gmail.com>
2023-03-29 07:33:47 +0400

    fix: useMountedFlag.ts

--- source/common/hooks/useMountedFlag.ts
+++ source/common/hooks/useMountedFlag.ts
@@ -1,6 +1,14 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 export function useMountedFlag() {
   const isMounted = useRef(true);
+
+  useEffect(
+    () => () => {
+      isMounted.current = false;
+    },
+    [],
+  );
+
   return useCallback(() => isMounted.current, []);
 }
